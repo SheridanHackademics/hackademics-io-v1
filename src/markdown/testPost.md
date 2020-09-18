3b2681cb935dea494be0e98971a44b61f6ebc936
Jacob Brasil <jnxtbrasil@gmail.com>
2020-09-17 23:39:04 -0400

    :sparkles: Added Markdown support

---
+++ src/markdown/testPost.md
@@ -0,0 +1,5 @@
+---
+slug: "/blog/test-post"
+date: "2020-09-17"
+title: "An Example Blog Post"
+---
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { graphql } from "gatsby"
+import React from "react"
+
+const IndexPage = () => {
+  return <div>👋 Hello Gatsby!</div>
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`
