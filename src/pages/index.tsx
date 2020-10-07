72613bf90164849575f328ae856d85a21dc85d11
Jacob Brasil <jnxtbrasil@gmail.com>
2020-10-07 18:06:43 -0400

    :lipstick: Starting to style site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,46 @@
-import { graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
+import Layout from "../components/layout"
+import Navbar from "../components/navbar/Navbar"
 
 const IndexPage = () => {
-  return <div>👋 Hello Gatsby!</div>
-}
-
-export default IndexPage
-
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          menuLinks {
+            name
             slug
-            title
           }
         }
       }
     }
-  }
-`
+  `)
+  
+  return (
+    <Layout>
+      <Navbar siteTitle={data.site.siteMetadata.title} menuLinks={data.site.siteMetadata.menuLinks}/>
+    </Layout>
+  );
+}
+
+export default IndexPage
+
+// export const pageQuery = graphql`
+//   query {
+//     allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+//       edges {
+//         node {
+//           id
+//           excerpt(pruneLength: 250)
+//           frontmatter {
+//             date(formatString: "MMMM DD, YYYY")
+//             slug
+//             title
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
