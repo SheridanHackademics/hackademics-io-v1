f5e7b9100137488251d6125a4e93ee9d49b12396
Jacob Brasil <jnxtbrasil@gmail.com>
2020-10-07 18:07:11 -0400

    :sparkles: Blog based off of .md Files

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import PostLink from "../components/post-link"
+import Layout from "../components/layout"
 
 const BlogPage = ({
   data: {
@@ -11,7 +12,7 @@ const BlogPage = ({
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-  return <div>{Posts}</div>
+  return <div><Layout>{Posts}</Layout></div>
 }
 
 export default BlogPage
