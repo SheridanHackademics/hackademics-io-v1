fccdd9a3bd287c1474f016bdde745c85507bfdbf
Jacob Brasil <jnxtbrasil@gmail.com>
2020-11-13 17:27:28 -0500

    Temporaily Removed Old Markdown -> Blog

--- src/markdown/testPost.md
+++
@@ -1,6 +0,0 @@
----
-slug: "/blog/test-post"
-date: "2020-09-17"
-title: "An Example Blog Post"
----
-This post is a test post used to test shit and ya y'know
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,36 +1,36 @@
-import React from "react"
-import { graphql } from "gatsby"
-import PostLink from "../components/post-link"
-import Layout from "../components/layout/layout"
+// import React from "react"
+// import { graphql } from "gatsby"
+// import PostLink from "../components/post-link"
+// import Layout from "../components/layout/layout"
 
-const BlogPage = ({
-  data: {
-    allMarkdownRemark: { edges },
-  },
-}) => {
-  const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+// const BlogPage = ({
+//   data: {
+//     allMarkdownRemark: { edges },
+//   },
+// }) => {
+//   const Posts = edges
+//     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+//     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-  return <div><Layout title="blog">{Posts}</Layout></div>
-}
+//   return <div><Layout title="blog">{Posts}</Layout></div>
+// }
 
-export default BlogPage
+// export default BlogPage
 
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            slug
-            title
-          }
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query {
+//     allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+//       edges {
+//         node {
+//           id
+//           excerpt(pruneLength: 250)
+//           frontmatter {
+//             date(formatString: "MMMM DD, YYYY")
+//             slug
+//             title
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
