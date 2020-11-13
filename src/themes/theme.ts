e8fc5dc9e9401ab2bc344d59e79d621ab8d640cf
Alec Di Vito <divito.alec@gmail.com>
2020-11-12 20:59:52 -0500

    Added theme details

---
+++ src/themes/theme.ts
@@ -0,0 +1,25 @@
+import { DefaultTheme } from "styled-components";
+
+export const defaultTheme: DefaultTheme = {
+    borderRadius: '4px',
+    palette: {
+        common: {
+            white: '#FFFFFF',
+            black: '#000000',
+            gray: '#707070',
+        },
+        uncommon: {
+            lightBlack: '#181818',
+            brightBlack: '#212121',
+        },
+        primary: {
+            red: '#EC4564',
+            blue: '#2952E5',
+            yellow: '#E2BF2D',
+            green: '#E2BF2D',
+        },
+        secondary: {
+            red: '#EA135B',
+        }
+    }
+}
