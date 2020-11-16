690428199ab5d709705117fba3dd71afad0a42cd
Jacob Brasil <jnxtbrasil@gmail.com>
2020-11-15 20:42:09 -0500

    Started making Divider & PageHero more responsive

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Divider = styled.div`
     border-top: 1rem solid #111;
     display: block;
-    width: '100%';
+    width: 100%;
     margin: 3rem 0px;
 `;
 
