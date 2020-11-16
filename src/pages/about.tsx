865e79c153430de3f0ac136f907fa468c8b77a1d
Jacob Brasil <jnxtbrasil@gmail.com>
2020-11-15 20:44:02 -0500

    About & Team page pull from Contentful

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,128 +1,153 @@
-import { graphql } from "gatsby";
-import React from "react";
-import Footer from "../components/footer/Footer";
-import { DefaultLayout, LandingLayout } from "../components/layouts";
-import Navbar from "../components/navbar/Navbar";
+import { graphql } from "gatsby"
+import React from "react"
+import Footer from "../components/footer/Footer"
+import { DefaultLayout } from "../components/layouts"
+import Navbar from "../components/navbar/Navbar"
 import Img from "gatsby-image"
-import Hero from "../components/hero";
-import Container from "../components/container";
-import styled from "styled-components";
-import { PrimaryColor } from "../themes/theme";
-import { Edges, IChildImageSharp, Node } from "../types";
+import Hero from "../components/hero"
+import Container from "../components/container"
+import styled from "styled-components"
+import { PrimaryColor } from "../themes/theme"
+import { Edges, IChildImageSharp, Node } from "../types"
+import SEO from "../components/seo"
 
 interface IProps {
-    data: {
-        site: {
-            siteMetadata: {
-                title: string,
-                menuLinks: {
-                    name: string,
-                    slug: string,
-                }[]
-            }
-        },
-        header: IChildImageSharp,
-        allPagesJson: Edges<Node<IAboutPage>>
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        menuLinks: {
+          name: string
+          slug: string
+        }[]
+      }
     }
-}
-
-interface IAboutPage {
-    title: string,
-    description: string,
-    items: IAboutSection[]
+    header: IChildImageSharp
+    allContentfulAboutPageItem: Edges<Node<IAboutSection>>
+  }
 }
 
 interface IAboutSection {
-    title: string,
-    titleColor: PrimaryColor,
-    description: string,
-    index: number,
-    image?: {
-        src: IChildImageSharp
-    }
+  id?: string
+  title: string
+  order: number
+  titleColor: {
+    hexColor: string
+  }
+  description: {
+    description: string
+  }
+  image?: {
+    fluid: any
+  }
 }
 
 const Section = styled.section<{ direction: boolean }>`
-    margin: 8rem 0rem 12rem;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    align-items: center;
-    text-align: ${props => props.direction ? "left" : "right"};
-    flex-direction: ${props => props.direction ? "row" : "row-reverse"};
-`;
+  margin: 8rem 0rem 12rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: ${props => (props.direction ? "left" : "right")};
+  flex-direction: ${props => (props.direction ? "row" : "row-reverse")};
 
-const SectionHeader = styled.h1<{ color: PrimaryColor }>` 
-    font-family: "Open Sans", sans-serif;
-    color: #2952E5;
-    letter-spacing: 5.5px;
-    text-transform: uppercase;
-    font-size: 55px;
-    margin-bottom: 2rem;
-    height: 75px;
-`;
+  @media (max-width: 788px) {
+    flex-flow: row wrap;
+    padding-left: ${props => (props.direction ? "5vw" : "0")};
+    padding-right: ${props => (props.direction ? "0" : "5vw")};
+  }
+`
 
-const SectionParagraph = styled.p`
-    font-family: "Open Sans", sans-serif;
-    font-size: 1.2em;
-`;
+const SectionHeader = styled.h1<{ color: string }>`
+  font-family: "Open Sans", sans-serif;
+  color: ${props => props.color};
+  letter-spacing: 5.5px;
+  text-transform: uppercase;
+  font-size: 55px;
+  margin-bottom: 2rem;
+  height: 75px;
+`
+
+const ItemText = styled.div``
+
+const SectionParagraph = styled.p<{ direction?: boolean }>`
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.2em;
+  max-width: 540px;
+`
 
 const SectionContent = styled.div<{ direction?: boolean }>`
-    flex: 1; /* additionally, equal width */
-    margin: ${props => (props.direction === null)
-        ? "0rem"
-        : (props.direction) ? "0rem 5rem 0rem 0rem" : "0rem 0rem 0rem 5rem"};
-`;
+  flex: 1; /* additionally, equal width */
+  margin: ${props =>
+    props.direction === null
+      ? "0rem"
+      : props.direction
+      ? "0rem 5rem 0rem 0rem"
+      : "0rem 0rem 0rem 5rem"};
+`
 
-const SquareImageHolder = styled(Img)`
-    flex: 1; /* additionally, equal width */
-    height: 540px;
-    width: 540px;
-`;
+const SquareImageHolder = styled.div`
+  height: 540px;
+  width: 540px;
+`
 
-const AboutSection = ({ title, titleColor, description, index, image }: IAboutSection) => {
+const AboutSection = ({
+  title,
+  titleColor,
+  description,
+  image,
+  order,
+}: IAboutSection) => {
 
-    let img = null;
-    if (image) {
-        img = <SquareImageHolder><Img fluid={image.src.childImageSharp.fluid} alt={title} /></SquareImageHolder>;
-    }
-    let isEven = index % 2 == 0;
-    if (index === 0) {
-        isEven = null
-    }
+  let isEven = order % 2 == 0
+  if (order === 0) {
+    isEven = null
+  }
 
-    return <Section direction={index % 2 == 0}>
-        <SectionContent direction={isEven}>
-            <SectionHeader color={titleColor}>{title}</SectionHeader>
-            <SectionParagraph>{description}</SectionParagraph>
-        </SectionContent>
-        {img}
+  return (
+    <Section direction={order % 2 == 0}>
+      <ItemText>
+      <SectionContent direction={isEven}>
+          <SectionHeader color={titleColor.hexColor}>{title}</SectionHeader>
+          <SectionParagraph direction={isEven}>{description.description}</SectionParagraph>
+      </SectionContent>
+      </ItemText>
+      {image != null ? (
+        <SquareImageHolder>
+          <Img fluid={image.fluid} alt={title} />
+        </SquareImageHolder>
+      ) : null}
     </Section>
-};
+  )
+}
 
 const AboutPage = ({ data }: IProps) => {
-    const { node } = data.allPagesJson.edges[0];
-    return (
-        <DefaultLayout>
-            <Navbar
-                useDark={true}
-                menuLinks={data.site.siteMetadata.menuLinks}
-            />
-            <Img fluid={data.header.childImageSharp.fluid} alt="header" />
-            <Container>
-                <Hero title={node.title} description={node.description} />
-                {node.items.map((a, i) =>
-                    <AboutSection title={a.title} titleColor={a.titleColor} description={a.description} index={i} image={a.image ?? null} />
-                )}
-            </Container>
-            <Footer menuLinks={data.site.siteMetadata.menuLinks}/>
-        </DefaultLayout>
-    )
+  return (
+    <DefaultLayout>
+      <SEO title="About" />
+      <Navbar useDark={true} menuLinks={data.site.siteMetadata.menuLinks} />
+      <Img fluid={data.header.childImageSharp.fluid} alt="header" />
+      <Container>
+        <Hero
+          title="About us"
+          description="Learn more about Hackademics and our events."
+        />
+        {data.allContentfulAboutPageItem.edges.map(item => (
+          <AboutSection
+            title={item.node.title}
+            titleColor={item.node.titleColor}
+            description={item.node.description}
+            order={item.node.order}
+            image={item.node.image}
+          />
+        ))}
+      </Container>
+      <Footer menuLinks={data.site.siteMetadata.menuLinks} />
+    </DefaultLayout>
+  )
 }
 
 export const query = graphql`
-query AboutPage {
+  query AboutPage {
     site {
       siteMetadata {
         title
@@ -132,36 +157,34 @@ query AboutPage {
         }
       }
     }
-    header: file(name: {eq: "Blue-Header"}, extension: {eq: "png"}) {
+    header: file(name: { eq: "Blue-Header" }, extension: { eq: "png" }) {
       childImageSharp {
-        fluid(pngQuality: 100) {
-          ...GatsbyImageSharpFluid
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
-    allPagesJson {
+    allContentfulAboutPageItem(sort: { fields: createdAt, order: ASC }) {
       edges {
         node {
-          description
-          title
-          items {
-            title
-            titleColor
+          description {
             description
-            image {
-              src {
-                childImageSharp {
-                  fluid {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
+          }
+          id
+          titleColor {
+            hexColor
+          }
+          image {
+            fluid {
+              ...GatsbyContentfulFluid_noBase64
             }
           }
+          title
+          order
         }
       }
     }
   }
 `
 
-export default AboutPage;
+export default AboutPage
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,158 +1,195 @@
-import { graphql } from "gatsby";
-import React from "react";
-import styled from "styled-components";
-import Layout from "../components/layout/layout";
-import Footer from "../components/footer/Footer";
-import Navbar from "../components/navbar/Navbar";
+import { graphql } from "gatsby"
+import React from "react"
+import styled from "styled-components"
+import { DefaultLayout } from "../components/layouts"
+import Footer from "../components/footer/Footer"
+import Navbar from "../components/navbar/Navbar"
 import Img from "gatsby-image"
-import Hero from "../components/hero";
-import Container from "../components/container";
-import { Edges, IChildImageSharp, Node } from "../types";
+import Hero from "../components/hero"
+import Container from "../components/container"
+import { Edges, IChildImageSharp, Node } from "../types"
+import SEO from "../components/seo"
 
 interface IProps {
-    data: {
-        site: {
-            siteMetadata: {
-                title: string,
-                menuLinks: {
-                    name: string,
-                    slug: string,
-                }[]
-            }
-        },
-        header: IChildImageSharp,
-        allPagesJson: Edges<Node<ITeamPage>>
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        menuLinks: {
+          name: string
+          slug: string
+        }[]
+      }
     }
+    header: IChildImageSharp
+    allContentfulTeam: Edges<Node<ITeam>>
+  }
 }
 
 interface ITeamPage {
-    title: string,
-    description: string,
-    executives: ITeamMember[],
-    teams: ITeam[],
+  title: string
+  description: string
+  executives: ITeamMember[]
+  teams: ITeam[]
 }
 
 interface ITeam {
-    title: string,
-    team: ITeamMember[],
+  id?: string
+  teamName: string
+  order?: number
+  featured?: boolean
+  team_member: ITeamMember[]
 }
 
 interface ITeamMember {
-    name: string,
-    title: string,
-    description: string,
-    image: {
-        src: IChildImageSharp
-    }
+  id?: string
+  fullName: string
+  biography: {
+    biography: string
+  }
+  photo: {
+    fluid: any
+  }
+  teamLead?: boolean
 }
 
 type Direction = "column" | "column-reverse" | "row" | "row-reverse"
 
-const Section = styled.section<{ textCentered?: boolean, direction: Direction }>`
-    margin: 8rem 0rem 12rem;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    flex-direction: ${props => props.direction};
-    text-align: ${props => props.textCentered ? "center" : "left"};
-`;
+const Section = styled.section<{
+  textCentered?: boolean
+  direction: Direction
+}>`
+  margin: 8rem 0rem 12rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: ${props => props.direction};
+  text-align: ${props => (props.textCentered ? "center" : "left")};
+`
 
-const SectionHeader = styled.h1` 
-    font-family: "Open Sans", sans-serif;
-    color: ${props => props.theme.palette.uncommon.lightBlack};
-    letter-spacing: 1.8px;
-    text-transform: uppercase;
-    font-size: 2.25em;
-    margin-bottom: 1rem;
-    height: 75px;
-`;
+const SectionHeader = styled.h1`
+  font-family: "Open Sans", sans-serif;
+  color: #181818;
+  letter-spacing: 1.8px;
+  text-transform: uppercase;
+  font-size: 2.25em;
+  margin-bottom: 1rem;
+  height: 75px;
+`
 
 const SectionParagraph = styled.p`
-    font-family: "Open Sans", sans-serif;
-    color: ${props => props.theme.palette.uncommon.lightBlack};
-    font-size: 1.5em;
-`;
+  font-family: "Open Sans", sans-serif;
+  color: #181818;
+  font-size: 1.5em;
+`
 
 const SectionContent = styled.div<{ direction?: boolean }>`
-    flex: 1; /* additionally, equal width */
-    padding-left: 7rem;
-`;
+  flex: 1; /* additionally, equal width */
+  padding-left: 7rem;
+`
 
 const SquareImage = styled.div`
-    flex: 1; /* additionally, equal width */
-    height: 480px;
-`;
+  width: 534px;
+  height: 534px;
+  margin-bottom: 2em;
+`
 
-const ExecutiveSection = ({ name, title, description, image }: ITeamMember) => (
-    <Section direction={"row-reverse"}>
+const ExecutiveSection = ({ teamName, team_member: members }: ITeam) => (
+  <>
+    {members.map(a => (
+      <Section direction={"row-reverse"}>
         <SectionContent>
-            <SectionHeader>{name} - {title}</SectionHeader>
-            <SectionParagraph>{description}</SectionParagraph>
+          <SectionHeader>
+            {a.fullName} - {teamName}
+          </SectionHeader>
+          <SectionParagraph>{a.biography.biography}</SectionParagraph>
         </SectionContent>
         <SquareImage>
-            <Img alt={`${name}, ${title}`}
-                fluid={image.src.childImageSharp.fluid}
-                style={{ height: '100%' }}
-                imgStyle={{ objectFit: 'contain' }} />
+          <Img
+            alt={`${name}, ${teamName}`}
+            fluid={a.photo.fluid}
+            imgStyle={{ objectFit: "cover" }}
+          />
         </SquareImage>
-    </Section>
-);
+      </Section>
+    ))}
+  </>
+)
 
 const TeamContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-row-gap: 6rem;
-    grid-column-gap: 16rem;
-`;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-row-gap: 6rem;
+  grid-column-gap: 16rem;
+`
 
 const TeamMemberContainer = styled.div`
-    width: '100%';
-`;
+  width: "100%";
+`
 
-const TeamSection = ({ title, team }: ITeam) => (
-    <Section direction={"column"} textCentered={true}>
-        <SectionHeader>{title}</SectionHeader>
-        <TeamContainer>
-            {team.map(a =>
-                <TeamMemberContainer key={a.name}>
-                    <Img alt={`${a.name}, ${a.title}`}
-                        fluid={a.image.src.childImageSharp.fluid}
-                        style={{ marginBottom: '2rem' }}
-                        imgStyle={{ objectFit: 'contain' }} />
-                    <SectionHeader>
-                        {a.title !== "Team Member" ? `${a.name} - ${a.title}` : a.name}
-                    </SectionHeader>
-                    <SectionParagraph>
-                        {a.description}
-                    </SectionParagraph>
-                </TeamMemberContainer>
-            )}
-        </TeamContainer>
-    </Section>
-);
+const TeamSection = ({ teamName, team_member: members }: ITeam) => (
+  <Section direction={"column"} textCentered={true}>
+    <SectionHeader>{teamName}</SectionHeader>
+    <TeamContainer>
+      {members.map(a => (
+        <TeamMemberContainer key={a.id}>
+          <SquareImage>
+          <Img
+            alt={`${a.fullName}`}
+            fluid={a.photo.fluid}
+            imgStyle={{ objectFit: "cover" }}
+          />
+          </SquareImage>
+          <SectionHeader>
+            {a.teamLead ? `${a.fullName} - Lead` : a.fullName}
+          </SectionHeader>
+          <SectionParagraph>{a.biography.biography}</SectionParagraph>
+        </TeamMemberContainer>
+      ))}
+    </TeamContainer>
+  </Section>
+)
 
 const TeamPage = ({ data }: IProps) => {
-    const { node } = data.allPagesJson.edges[1];
-    return (
-        <Layout title='about'>
-            <Navbar
-                siteTitle={data.site.siteMetadata.title}
-                menuLinks={data.site.siteMetadata.menuLinks}
+  var teams = data.allContentfulTeam.edges
+
+  teams.map((team: Node<ITeam>) => {
+    team.node.team_member.sort((a, b) => (a.teamLead > b.teamLead ? -1 : +1))
+  })
+
+  return (
+    <DefaultLayout>
+      <SEO title="Team" />
+      <Navbar useDark={true} menuLinks={data.site.siteMetadata.menuLinks} />
+      <Img fluid={data.header.childImageSharp.fluid} alt="header" />
+      <Container>
+        <Hero title="The Team" description="Meet the Hackademics team." />
+
+        {teams.map((team: Node<ITeam>) => (
+          
+          team.node.featured ? (
+            <ExecutiveSection
+              key={team.node.id}
+              teamName={team.node.teamName}
+              team_member={team.node.team_member}
             />
-            <Img fluid={data.header.childImageSharp.fluid} alt="header" />
-            <Container>
-                <Hero title={node.title} description={node.description} />
-                {node.executives.map(a => <ExecutiveSection key={a.name} name={a.name} title={a.title} description={a.description} image={a.image} />)}
-                {node.teams.map(a => <TeamSection key={a.title} title={a.title} team={a.team} />)}
-            </Container>
-            <Footer />
-        </Layout>
-    )
+          ) : (
+            <TeamSection
+              key={team.node.id}
+              teamName={team.node.teamName}
+              team_member={team.node.team_member}
+            />
+          )
+        ))}
+      </Container>
+      <Footer menuLinks={data.site.siteMetadata.menuLinks} />
+    </DefaultLayout>
+  )
 }
 
 export const query = graphql`
-query TeamPage {
+  query TeamPage {
     site {
       siteMetadata {
         title
@@ -162,53 +199,39 @@ query TeamPage {
         }
       }
     }
-    header: file(name: {eq: "Red-Header"}, extension: {eq: "png"}) {
+    header: file(name: { eq: "Red-Header" }, extension: { eq: "png" }) {
       childImageSharp {
-        fluid(pngQuality: 100) {
-            ...GatsbyImageSharpFluid
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
-    allPagesJson {
+    allContentfulTeam(
+      sort: { fields: team_member___team___order, order: ASC }
+    ) {
       edges {
         node {
-          description
-          title
-          executives {
-            name
-            title
-            description
-            image {
-              src {
-                childImageSharp {
-                  fluid(maxWidth: 500, maxHeight: 500) {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
+          id
+          teamName
+          order
+          featured
+          team_member {
+            id
+            fullName
+            biography {
+              biography
+            }
+            photo {
+              fluid(maxWidth: 534, maxHeight: 534) {
+                ...GatsbyContentfulFluid_noBase64
               }
             }
-          }
-          teams {
-            title
-            team {
-              name
-              title
-              description
-              image {
-                src {
-                  childImageSharp {
-                    fluid(maxWidth: 500, maxHeight: 500) {
-                        ...GatsbyImageSharpFluid
-                    }
-                  }
-                }
-              }
-            }
+            teamLead
           }
         }
       }
     }
-  }  
+  }
 `
 
-export default TeamPage;
+export default TeamPage
