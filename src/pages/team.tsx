efa6b55595bfc45e12b6e9b9fd06bb7d0d83396d
Alec Di Vito <divito.alec@gmail.com>
2020-11-17 22:17:00 -0500

    Made team page resizable

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -49,54 +49,111 @@ interface ITeamMember {
 
 type Direction = "column" | "column-reverse" | "row" | "row-reverse"
 
-const Section = styled.section<{
-  textCentered?: boolean
-  direction: Direction
-}>`
-  margin: 8rem 0rem 12rem;
+const Section = styled.section<{ textCentered?: boolean, mobileDirection?: Direction, direction: Direction }>`
+  margin: 8rem 0rem 12rem 0rem;
   width: 100%;
   display: flex;
   justify-content: center;
   align-content: center;
   flex-direction: ${props => props.direction};
   text-align: ${props => (props.textCentered ? "center" : "left")};
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    margin: 6rem 0rem 0rem 0rem;
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    margin: 2rem 0rem 0rem 0rem;
+    flex-direction: ${props => props.mobileDirection ?? props.direction};
+  }
 `
 
-const SectionHeader = styled.h1`
-  font-family: "Open Sans", sans-serif;
+const Header = styled.h2`
+  font-family: ${props => props.theme.text.font};
   color: ${props => props.theme.palette.uncommon.lightBlack};
   letter-spacing: 1.8px;
   text-transform: uppercase;
-  font-size: 2.25em;
   margin-bottom: 1rem;
   height: 75px;
-`
+`;
+
+const TeamHeader = styled(Header)`
+  font-size: 2.25em;
+  margin-top: 4rem;
+`;
+
+const MemberHeader = styled(Header)`
+  font-size: 2em;
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    height: 50px;
+    font-size: 1.5em; 
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    height: 50px;
+    font-size: 1.4em; 
+  }
+`;
 
 const SectionParagraph = styled.p`
-  font-family: "Open Sans", sans-serif;
+  font-family: ${props => props.theme.text.font};
   color: ${props => props.theme.palette.uncommon.lightBlack};
   font-size: 1.5em;
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    font-size: 1.25em; 
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    text-align: left;
+  }
 `
 
 const SectionContent = styled.div<{ direction?: boolean }>`
   flex: 1; /* additionally, equal width */
   padding-left: 7rem;
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    padding-left: 3.5rem;
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    text-align: center;
+    padding: 1rem;
+    margin-bottom: 2rem;
+  }
 `
 
 const SquareImage = styled.div`
   width: 534px;
   height: 534px;
   margin-bottom: 2em;
-`
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    width: 35%;
+    height: 35%;
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.mobile}) {
+    width: 100%; 
+  }
+`;
+
+const SquareGridImage = styled(SquareImage)`
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    width: 100%;
+  }
+`;
 
 const ExecutiveSection = ({ teamName, team_member: members }: ITeam) => (
   <>
     {members.map(a => (
-      <Section direction={"row-reverse"}>
+      <Section direction={"row-reverse"} mobileDirection={"column-reverse"}>
         <SectionContent>
-          <SectionHeader>
+          <MemberHeader>
             {a.fullName} - {teamName}
-          </SectionHeader>
+          </MemberHeader>
           <SectionParagraph>{a.biography.biography}</SectionParagraph>
         </SectionContent>
         <SquareImage>
@@ -116,28 +173,38 @@ const TeamContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   grid-row-gap: 6rem;
   grid-column-gap: 16rem;
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    grid-row-gap: 2rem;
+    grid-column-gap: 8rem;
+  }
+
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    grid-template-columns: 1fr;
+  }
 `
 
 const TeamMemberContainer = styled.div`
-  width: "100%";
+  width: 100%;
+  height: fit-content;
 `
 
 const TeamSection = ({ teamName, team_member: members }: ITeam) => (
   <Section direction={"column"} textCentered={true}>
-    <SectionHeader>{teamName}</SectionHeader>
+    <TeamHeader>{teamName}</TeamHeader>
     <TeamContainer>
       {members.map(a => (
         <TeamMemberContainer key={a.id}>
-          <SquareImage>
-          <Img
-            alt={`${a.fullName}`}
-            fluid={a.photo.fluid}
-            imgStyle={{ objectFit: "cover" }}
-          />
-          </SquareImage>
-          <SectionHeader>
+          <SquareGridImage>
+            <Img
+              alt={`${a.fullName}`}
+              fluid={a.photo.fluid}
+              imgStyle={{ objectFit: "cover" }}
+            />
+          </SquareGridImage>
+          <MemberHeader>
             {a.teamLead ? `${a.fullName} - Lead` : a.fullName}
-          </SectionHeader>
+          </MemberHeader>
           <SectionParagraph>{a.biography.biography}</SectionParagraph>
         </TeamMemberContainer>
       ))}
@@ -161,7 +228,7 @@ const TeamPage = ({ data }: IProps) => {
         <Hero title="The Team" description="Meet the Hackademics team." />
 
         {teams.map((team: Node<ITeam>) => (
-          
+
           team.node.featured ? (
             <ExecutiveSection
               key={team.node.id}
@@ -169,12 +236,12 @@ const TeamPage = ({ data }: IProps) => {
               team_member={team.node.team_member}
             />
           ) : (
-            <TeamSection
-              key={team.node.id}
-              teamName={team.node.teamName}
-              team_member={team.node.team_member}
-            />
-          )
+              <TeamSection
+                key={team.node.id}
+                teamName={team.node.teamName}
+                team_member={team.node.team_member}
+              />
+            )
         ))}
       </Container>
       <Footer menuLinks={data.site.siteMetadata.menuLinks} />
